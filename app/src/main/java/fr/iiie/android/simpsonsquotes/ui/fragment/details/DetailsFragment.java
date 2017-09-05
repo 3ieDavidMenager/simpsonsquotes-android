0bf0f103f75d90c512b5c1fc407ae442fb7f1bea
Ronan Cendrier <ronan.cendrier@3ie.fr>
2017-09-05 13:38:49 +0200

    Added large image in quote details

--- app/src/main/java/fr/iiie/android/simpsonsquotes/ui/fragment/details/DetailsFragment.java
+++ app/src/main/java/fr/iiie/android/simpsonsquotes/ui/fragment/details/DetailsFragment.java
@@ -6,7 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,15 +26,27 @@ public class DetailsFragment extends Fragment
     @BindView(R.id.fragment_details_timestampText)
     TextView timestampText;
 
+    @BindView(R.id.fragment_details_largeImage)
+    ImageView largeImage;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState)
     {
         final View rootView = inflater.inflate(R.layout.fragment_details, container, false);
+
         ButterKnife.bind(this, rootView);
-        idText.setText(this.getArguments().getString("id"));
-        episodeText.setText(this.getArguments().getString("episode"));
-        timestampText.setText(this.getArguments().getString("timestamp"));
+
+        String episode = this.getArguments().getString("episode");
+        String timestamp = this.getArguments().getString("timestamp");
+        String id = this.getArguments().getString("id");
+
+        episodeText.setText("Episode: " + episode);
+        timestampText.setText("Timestamp" + timestamp);
+        idText.setText("Id: " + id);
+
+        String image_url = "https://frinkiac.com/img/" + episode + "/" + timestamp + "/large.jpg";
+        Glide.with(this).load(image_url).into(largeImage);
         return rootView;
     }
 }
